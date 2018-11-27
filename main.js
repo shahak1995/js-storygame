46b087d9f45925aa841e6dca8e762335d041a166
Tech-A24 <tech-a24@Tech-A24s-MacBook-Air.local>
2018-11-27 23:09:10 +0000

    Did alert and prompt to tell the user to enter their name to play the game

---
+++ main.js
@@ -0,0 +1,15 @@
+alert("The HAUNTING!");
+const person = prompt("Enter your name if you DARE!");
+if (person === "shah") {
+  alert("You are a VERY brave HUMAN!");
+} else {
+  alert("You are a WUSS!");
+}
+
+
+
+
+
+
+
+
