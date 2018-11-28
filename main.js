46e5e4346f3a775af2d282d95b2fd10a5293f866
Tech-A24 <tech-a24@Tech-A24s-MacBook-Air.local>
2018-11-28 00:09:24 +0000

    Added two more prompts and if and else statements for different decisions the user chooses to make which results in different outputs

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 alert("The HAUNTING!");
-const person = prompt("Enter your name if you DARE!");
-if (person === "shah") {
+const name = prompt("Enter your name if you DARE!");
+if (name === "shah") {
   alert("You are a VERY brave HUMAN!");
 } else {
   alert("You are a WUSS!");
+}
+
+var house = prompt("Do you want to enter the house?");
+if (house === "yes") {
+  alert("You are MAD!");
+} else {
+  alert("You still going to go in :)");
+}
+
+var voice = prompt("You hear a voice on the other side of the house which is straight!. Do you go straight to that direction or go right and enter the corridor?");
+if (voice === "straight") {
+  alert("Demon came out and killed you leaving you decapitated!");
+} else {
+  alert("Good Choice! you have now entered the corridor");
 }
 
 
